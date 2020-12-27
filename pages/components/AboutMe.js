197e73f17dbce4e22adb68021a3b5a90ffd0a379
rahulmanjunath <yajat472001@gmail.com>
2020-12-27 21:56:22 +0530

    completed about page

--- pages/components/AboutMe.js
+++ pages/components/AboutMe.js
@@ -6,7 +6,6 @@ const AboutMe = () => {
   return (
     <div className={style.container}>
       <div className={style.title}>About Me</div>
-
       <div className={style.content}>
         Hey there, I am so glad you are here. I am Yajat Vishwakarma. I am a
         programmer who loves to innovate. I appreciate technology and
@@ -15,6 +14,10 @@ const AboutMe = () => {
         helping you "scale a kubernetes cluster". This site is my portfolio
         which comprises of my works, achievements and failures. Looking forward
         to see you in my DMs soon!
+        <img
+          className={style.picture}
+          src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        ></img>
       </div>
       <div className={style.contactMe}>
         <ContactButton />
